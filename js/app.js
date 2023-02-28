cfdfe2fc21d0e9b1370300c00a1e67fb27a684f8
Natalia Stave <Natalia.stave20@gmail.com>
2023-02-28 05:39:01 +0100

    terminamos

--- js/app.js
+++ js/app.js
@@ -6,10 +6,13 @@ if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
 }
 
 checkbox.addEventListener('change',function (event) {
- console.log(this.checked)
- if (this.checked) {
-    document.body.classList.add('is-dark-mode')
- }  else { document.body.classList.remove('is-dark-mode')
-    
- }
+document.body.classList.toggle('is-dark-mode') 
+
+if (this.checked) {
+  document.body.classList.remove('is-light-mode')
+  document.body.classList.add('is-dark-mode')
+} else {
+    document.body.classList.remove('is-dark-mode')
+    document.body.classList.add('is-light-mode')
+}
 })
